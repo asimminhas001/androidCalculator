cef6a0d5adb5e2559376d20b87bf4b3c38686c66
Moe Hamoud <m.hamoudd@gmail.com>
2016-12-26 01:18:21 -0500

    Added demo colorpicker to settings screen which allows changing the settings toolbar background

--- app/src/main/java/calc/ui/settings/SettingsView.java
+++ app/src/main/java/calc/ui/settings/SettingsView.java
@@ -1,8 +1,10 @@
 package calc.ui.settings;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -10,6 +12,10 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import kamal.calculator.R;
+import uz.shift.colorpicker.LineColorPicker;
+import uz.shift.colorpicker.OnColorChangedListener;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by mhamoud on 2016-12-24.
@@ -32,10 +38,32 @@ public final class SettingsView extends LinearLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
 
+        final Toolbar toolbar = ButterKnife.findById(this, R.id.toolbar);
         TextView toolbarTitle = ButterKnife.findById(this, R.id.toolbar_title);
         ImageView toolbarSettingsBtn = ButterKnife.findById(this, R.id.toolbar_settings_btn);
+        LineColorPicker colorPicker = ButterKnife.findById(this, R.id.picker);
 
         toolbarTitle.setText(R.string.settings_screen_name);
         toolbarSettingsBtn.setVisibility(GONE);
+
+        // set color palette
+        colorPicker.setColors(new int[]{Color.RED, R.color.colorPrimary, Color.BLUE, Color.YELLOW});
+
+        // set selected color [optional]
+        colorPicker.setSelectedColor(R.color.colorPrimary);
+
+        // set on change listener
+        colorPicker.setOnColorChangedListener(new OnColorChangedListener() {
+            @Override
+            public void onColorChanged(int c) {
+                Log.d(TAG, "Selected color " + Integer.toHexString(c));
+
+                // set toolbar color
+                toolbar.setBackgroundColor(c);
+            }
+        });
+
+        // get selected color
+        int color = colorPicker.getColor();
     }
 }
